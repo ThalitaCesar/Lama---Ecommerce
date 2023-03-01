924c8ba11f7013922369f8129fe1319be754ceb0
ThalitaCesar <thalita.walleska@gmail.com>
2023-03-01 14:44:21 -0300

    ..

--- Client/src/screens/Car/CardRequest/index.jsx
+++ Client/src/screens/Car/CardRequest/index.jsx
@@ -1,12 +1,32 @@
 import { Button, IconButton } from '@material-ui/core';
 import { Delete } from '@material-ui/icons';
-import React from 'react'
+import axios from 'axios';
+import React, { useContext, useEffect } from 'react'
 import { useState } from 'react';
+import { GlobalContext } from '../../../context/GlobalState';
 import { Card, ImgContainer, NameProduct, Quantity, Tam, Value } from './styles';
 
 function CardRequest() {
-const [quantity, setQuantity] = useState(0)
 
+const [quantity, setQuantity] = useState(1)
+const [order, setOrder] = useState([]);
+const [subTotal, setSubTotal] = useState();
+const {userId} = useContext(GlobalContext)
+
+const getOrders =()=>{
+  axios.get(`http://localhost:3003/order/getorderbyuser/${userId}`)
+  .then(function (response) {
+    setOrder(response.data.Result)
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+}
+
+useEffect(()=>{
+  getOrders()
+},[order])
 
 const handleIncrease=()=>{
   setQuantity(quantity + 1);
@@ -19,28 +39,37 @@ const handleDecrease = () => {
 }
 };
 
-//reset counter 
-const handleReset = () =>{
-  setQuantity(0)
+//reset order 
+const handleReset = (order) =>{
+  axios.delete(`http://localhost:3003/order/deleteorder/${order.id}`)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
 }
 
+console.log("order", order)
 
     return (
 <>
-<Card>
+{order.map((order)=>(
+<Card key={order.id}>
 <div>
-  <ImgContainer background={'https://img.ltwebstatic.com/images3_pi/2021/11/19/1637284725d9569a24e6f415912bb191d03569ac78_thumbnail_600x.webp'}/>
+  <ImgContainer background={order.folder}/>
   </div>
-  <NameProduct>Vestido V Vita Rosa</NameProduct>
-  <Tam> P</Tam>
+  <NameProduct>{order.name}</NameProduct>
+  <Tam>{order.size}</Tam>
   <Quantity>
     <Button onClick={handleDecrease}>-</Button>
    {quantity}
     <Button onClick={handleIncrease}>+</Button>
   </Quantity>
-  <Value> R$ 299,90</Value>
-  <IconButton onClick={handleReset}><Delete/></IconButton>
+  <Value >R${parseFloat(order.price.substring(2).replace(',', '.'))*quantity}</Value>
+  <IconButton onClick={()=>handleReset(order)}><Delete/></IconButton>
 </Card>
+))}
 </>
   )} 
   
